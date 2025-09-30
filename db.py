b0c300a5c1d6d7b64dd78eb6fd2793146f3d10aa
Murilo DIonizio <murilomelodionizio@gmail.com>
2025-09-30 16:48:57 -0300

    Criando a função criar aluno

--- db.py
+++ db.py
@@ -12,3 +12,20 @@ params = {
     "port": os.getenv("DB_PORT"),
 
 }
+
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
+
+conexao, cursor = conectar()
+cursor.execute(
+    "INSERT INTO alunos (nome, idade) VALUES (%s, %s)",
+    ("Murilo", 33)
+    )
+conexao.commit()
+conexao.close()
